c905a9e370ea9669050ffd82e867f51edccc8bba
NadiaRBT <149373205+NadiaRBT@users.noreply.github.com>
2023-12-05 10:42:00 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
 
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project was coded by Nadia Russell-Brett and is {" "}
-        <a
-          href="https://github.com/NadiaRBT/weatherproject"
-          target="_blank"
-        >open-sourced on GitHub</a>
-      </footer>
+      <div className="container">
+        Weather app
+        <footer>
+          This project was coded by Nadia Russell-Brett and is{" "}
+          <a href="https://github.com/NadiaRBT/weatherproject" target="_blank">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-
