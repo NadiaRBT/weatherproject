48ece9a21a0e31dcfdc6dddb6fd81fb432385eee
NadiaRBT <149373205+NadiaRBT@users.noreply.github.com>
2023-12-06 13:31:46 +0200

    Used correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -15,16 +15,16 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
-      iconUrl: `https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png`,
+      iconUrl: ` https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
   }
 
-  function search(){
-  const apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
+  function search() {
+    const apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleSubmit(event) {
@@ -39,7 +39,7 @@ export default function Weather(props) {
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form onSubmit ={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -59,7 +59,7 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <WeatherInfo  data={weatherData}/>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
